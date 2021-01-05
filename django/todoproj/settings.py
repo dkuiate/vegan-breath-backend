28046b629c9cb03acba9316c78e612a484d24844
dkuiate <danelkatel.kuiatetagne@canal-plus.com>
2021-01-05 14:36:23 +0100

    saving dev

--- django/todoproj/settings.py
+++ django/todoproj/settings.py
@@ -140,12 +140,14 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
+        #'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        
     ),
     'NON_FIELD_ERRORS_KEY': 'global',
 }
--- django/vegan/models.py
+++ django/vegan/models.py
@@ -9,7 +9,7 @@ class Contact(models.Model):
 class Recette(models.Model):
   title = models.CharField(max_length=200, unique=True)
   description = models.TextField()
-  picture = models.URLField()
+  picture = models.ImageField(upload_to = 'recettes')
   created_at= models.DateTimeField(auto_now_add=True)
   def __str__(self):
         return self.title
@@ -17,9 +17,11 @@ class Recette(models.Model):
 
 class Restaurant(models.Model):
   title = models.CharField(max_length=200, unique=True)
-  description = models.TextField()
   email = models.EmailField(max_length=100)
+  description = models.TextField()
   created_at= models.DateTimeField(auto_now_add=True)
+  picture = models.URLField(max_length=200)
+  #picture = models.ImageField(upload_to = 'restaurants')
   def __str__(self):
         return self.title
 
@@ -29,6 +31,7 @@ class Shop(models.Model):
   description = models.TextField()
   email = models.EmailField(max_length=100)
   created_at= models.DateTimeField(auto_now_add=True)
+  picture = models.URLField(max_length=200)
   def __str__(self):
         return self.title
 
